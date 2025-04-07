3ca5f2097b068a1ab97de554a249910c67809e16
RedyRep11 <alessandro.benedetti45@gmail.com>
2025-04-07 20:11:36 +0200

    Fix Next.js config for Cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export", // Required for static export
+  reactStrictMode: true, // Optional: enables React strict mode
+  images: {
+    unoptimized: true, // Disables Next.js image optimization
+  },
+  // (Optional) Add trailing slashes to URLs
+  trailingSlash: true,
 };
 
 export default nextConfig;
